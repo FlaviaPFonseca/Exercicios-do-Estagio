79e5ab695a1d53519e3790e8a421c8c81856e2c3
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-05-31 23:32:56 -0300

    Add files via upload
    projeto final ,corrigido erro na geração de mostrar os cálculos da tabuada.

---
+++ Tabuada/script.js
@@ -0,0 +1,18 @@
+function tabuada (){
+    let num =document.getElementById('txtn')
+    let tab =document.getElementById('seltab')
+if(num.value.length == 0){
+    window.alert('Por favor, digite um numero!')
+   }else{
+    let n = Number(num.value)
+    let c =1
+    tab.innerHTML = ''
+    while (c <= 10){
+        let item = document.createElement('option')
+         item.text = `${n} x ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+        c++
+    }
+}
+}
