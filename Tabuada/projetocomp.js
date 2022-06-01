861c8de455cef2cd8ba66dff0f35473fbde8128f
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-05-31 21:51:34 -0300

    Add files via upload
    Base  do gerador de tabuada .

---
+++ Tabuada/projetocomp.js
@@ -0,0 +1,9 @@
+function tabuada (){
+    let num =document.getElementById('txtn')
+    let tab =document.getElementById('seltab')
+if(num.value.length == 0){
+    window.alert('Por favor, digite um numero!')
+   }else{
+    let n = Number(num.value)
+}
+}
