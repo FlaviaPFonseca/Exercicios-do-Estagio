ffaa2310985d6d6dfe5ec715af9472168d5222d4
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-06-04 23:35:26 -0300

    Add files via upload
    .js  acrescentando timer, corrigindo linhas duplicadas;
    .html  acréscimo do histórico do tempo de jogo;
    .css  alteração da cor do timer quando o jogo iniciasse;

--- Memória/memoria.js
+++ Memória/memoria.js
@@ -1,23 +1,24 @@
 //cartas
 let imagens =[];
-for (let i=1;i<=8; i++) imagens.push(`http://picsum.photos/id/${i}/80`);
-let fundo ='https://picsum.photos/80?grayscale';
+for (let i=1; i<=8; i++) imagens.push(`http://picsum.photos/id/${i}/80`);
+let fundo ='https://picsum.photos/90?grayscale';
  
-// Estado do jogo
+// Elementos do estado do jogo
 let cartas =[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 let cliquesTravados = false;
 let temCartaVirada = false;
 let posicaoCartaVirada = -1;
 let valorCartaVirada = 0;
+const timerDoJogo = new Timer('#timer');
 
 onload = () => {
-
-    // carrega as imagens de fundo
+// carrega as imagens de fundo
 let elemImagens= document.querySelectorAll('#memoria img');
-elemImagens.forEach ((img,i) =>{
-       img.src = fundo; 
-       img.setAttribute('data-valor',i);
-       img.style.opacity = 0.5;
+elemImagens.forEach (
+    (img,i) =>{
+    img.src = fundo; 
+    img.setAttribute('data-valor',i);
+    img.style.opacity = 0.5;
     });
 
     //cria evento do botao inicio
@@ -32,15 +33,16 @@ const iniciaJogo = () =>{
     for (let i=0; i<cartas.length;i++){
         let p = Math.trunc(Math.random() * cartas.length)
        let aux = cartas[p];
-       cartas[p]=cartas [i];
+       cartas[p] = cartas[i];
        cartas[i]=aux;
     }
 
     // Associar evento a imagem
     let elemImagens =document.querySelectorAll('#memoria img');
-    elemImagens.forEach((img,i)=>{
+    elemImagens.forEach((img,i)=> {
         img.onclick = trataCliqueImagem;
         img.style.opacity = 1;
+        img.src= fundo;
     });
 
     //reinicia o jogo
@@ -52,7 +54,10 @@ const iniciaJogo = () =>{
 
     // bloqueia botao inciar apos o jogo comecar.
     document.querySelector('#btInicio').disable = true;
+    document.querySelector('#timer').style.backgroundColor ='orange';
+    timerDoJogo.start();
 };
+
 
 //-------------------------
 // clique na imagem pra virar a carta
@@ -75,9 +80,9 @@ const trataCliqueImagem =(e)=>{
         } else{
             cliquesTravados = true;
             setTimeout( ()=>{
-            e.target.src = fundo ;
+            e.target.src = fundo;
             e.target.onclick = trataCliqueImagem;
-            let img = document.querySelector('#memoria #i+'posicaoCartaVirada);
+            let img = document.querySelector('#memoria #i'+p0);
             img.src = fundo;
             img.onclick = trataCliqueImagem;
             cliquesTravados = false;
@@ -87,16 +92,35 @@ const trataCliqueImagem =(e)=>{
         posicaoCartaVirada = -1;
         valorCartaVirada = 0;
     }
+            if(pontos == 8){
+               document.querySelector('#btInicio').disable = false; 
+               document.querySelector('#timer').style.backgroundColor='ligthgreem';
+               timerDoJogo.stop();
+            }
+        };
+//-------------------------
+// Timer- Contador de tempo
+//-------------------------
+function Timer(e){
+    this.element =e;
+    this.time =0;
+    this.control = null;
+    this.start =() => {
+        thid.time=0;
+     this.control = setInterval( ()=>{
+         this.time ++;
+         const minutes = math.trunc(this.time/60);
+         const seconds =this.time % 60;
+         document.queryCommandIndeterm(this.element).innerHTML =
+         (minuts <10 ? '0':'')+  minutes+
+         ':'+
+         (seconds <10 ? '0':'')+ 
+         seconds;
+     }, 1000 );
+    };
+    this.stop =() => {
+    clearInterval(this.control);
+    };
+
     
-
-    cliquesTravados = true;
-
-   setTimeout( ()=>{
-        e.target.src = fundo ;
-        e.target.onclick = trataCliqueImagem;
-        cliquesTravados = false;
-    },1500);
-    if(pontos ==8){
-        document.querySelector('#btInicio').disable = false;
-    }
 }
