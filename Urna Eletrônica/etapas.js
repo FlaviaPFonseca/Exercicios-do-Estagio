5e92b6ca0a1d639004d02724d96e5e15295224a5
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-06-10 23:27:43 -0300

    Add files via upload
    Foto dos candidatos, 
    criação das etapas do .JS
    alteração do CSS

---
+++ Urna Eletrônica/etapas.js
@@ -0,0 +1,29 @@
+let etapas= [
+    {
+    titulo:'VEREADOR'
+    numeros: 5,
+candidatos: [
+    {
+        numero: 38111,
+        name:'Bertano de Tal'
+        partido:'ABC'
+        fotos:[
+            { url:38111.jpg, legenda:'Vereador',small:false }
+        ]
+    },
+    {
+        numero: 77222,
+        name:'Jose de Oliveira'
+        partido:'DVD'
+        fotos:[
+            { url:77222.jpg, legenda:'Vereador',small:false }
+        ]   
+    },
+]
+    },
+    {
+        titulo:'PREFEITO'
+        numeros: 2,
+    candidatos:     
+    }
+]
