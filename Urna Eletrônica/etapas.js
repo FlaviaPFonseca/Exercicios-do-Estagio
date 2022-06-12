aabb0f45d787daceae6aee46e828608421baadeb
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-06-11 23:51:21 -0300

    Add files via upload

--- Urna Eletrônica/etapas.js
+++ Urna Eletrônica/etapas.js
@@ -1,20 +1,26 @@
 let etapas= [
     {
     titulo:'VEREADOR'
-    numeros: 5,
+        numeros: 5,
+get numeros() {
+        return this.numeros;
+    },
+set numeros(value) {
+        this.numeros = value;
+    },
 candidatos: [
     {
         numero: 38111,
-        name:'Bertano de Tal'
-        partido:'ABC'
+        name:'Bertano de Tal',
+        partido:'ABC',
         fotos:[
             { url:38111.jpg, legenda:'Vereador',small:false }
         ]
     },
     {
         numero: 77222,
-        name:'Jose de Oliveira'
-        partido:'DVD'
+        name:'Jose de Oliveira',
+        partido:'DVD',
         fotos:[
             { url:77222.jpg, legenda:'Vereador',small:false }
         ]   
@@ -22,8 +28,28 @@ candidatos: [
 ]
     },
     {
-        titulo:'PREFEITO'
+        titulo:'PREFEITO',
         numeros: 2,
-    candidatos:     
-    }
+    candidatos: [
+        {
+            numero: 99,
+            name:'Ciclano',
+            partido:'DVD',
+            vice:'Ciclano de Souza',
+            fotos:[
+                { url:'xxxx.jpg', legenda:'Prefeito'},
+                { url:'1xxxx.jpg', legenda:'Vice-Prefeito',small:false }
+            ]   
+        },
+{
+    numero: 48,
+    name:'Jose Ciclano',
+    partido:'DVD',
+    vice:'Maria Ciclana de Souza',
+    fotos:[
+        { url:'4xxxx.jpg', legenda:'Prefeito'},
+        { url:'3xxxx.jpg', legenda:'Vice-Prefeito',small:false }
+    ]   
+},  
 ]
+    }
