54e6ec4ea76f5de46cb3b71e64b783180666c2ad
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-06-19 22:41:39 -0300

    Add files via upload
    criação do arquivo .js
    Alteração no arquivo .html
    alteração correspondente no arquivo. css

--- Calculadora imc/imc.js
+++ Calculadora imc/imc.js
@@ -1,13 +1,34 @@
-const calcular = document.getElementById('calcular') //VAI ATRIBUIR VALOR//
+const calcular = document.getElementById('calcular')
 
 function imc() {
-  const nome = document.getElementById('nome').value //.VALUE = nome altura e peso =>PRECISA DO VALOR DIGITADO PARA O CODIGO FUNCIONAR
+  const nome = document.getElementById('nome').value
   const altura = document.getElementById('altura').value
   const peso = document.getElementById('peso').value
-  const resultado = document.getElementById('resultado') //VAI ATRIBUIR VALOR//
+  const resultado = document.getElementById('resultado')
 
-  if (nome !== '') {
-    alert('valor preenchido!')
+  if (nome !== '' && altura !== '' && peso !== '') {
+    const valorIMC = (peso / (altura * altura)).toFixed(1)
+
+    let classificacao = ''
+
+    if (valorIMC < 18.5) {
+      classificacao = 'baixo peso'
+    } else if (valorIMC < 25) {
+      classificacao = 'Peso Ideal! Parabéns!!'
+    } else if (valorIMC < 30) {
+      classificacao = 'Alerta de Sobrepeso!!'
+    } else if (valorIMC < 35) {
+      classificacao = 'Obesidade Grau I!!'
+    } else if (valorIMC < 40) {
+      classificacao = 'Obesidade Grau II!!'
+    } else {
+      classificacao =
+        'Obesidade Grau III. Cuidado procure ajuda médica e psicológica!!'
+    }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
+   } else {
   }
 }
-calcular.addEventListener('click', imc)
+
+calcular.addEventListener('click', imc);
